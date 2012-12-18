a4155984479ecbe6e4e6bc7bfab74ecc44419bf6
saasbook <saasbook@saasbook.(none)>
2012-12-18 10:13:23 -0800

    Application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondhandCars::Application.config.secret_token = '0f9fa9ea4577ffacecb8e6ded589ac03ba6a759a87f4e2b377c628898642b35af8ee5f4911af02c69400c230f62b37807dea74dc4d470f0b05a89895e07d336e'

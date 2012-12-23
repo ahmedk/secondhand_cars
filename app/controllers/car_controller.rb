d357114c7395d410c3c403dc13bd76e2257ac979
ahmedk <ahmed.m.kamel@nileu.edu.eg>
2012-12-23 12:55:27 -0800

    Added single car display

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -4,5 +4,15 @@ class CarController < ApplicationController
   end
 
   def details
+    id = params[:id]
+    if id.nil?
+      flash[:error] = "Missing data"
+      redirect_to(:action => :index)
+    end
+    @car = Car.find(id)
+    if(@car.nil?)
+      flash[:error] = "No such car"
+      redirect_to :action => :index
+    end
   end
 end

9f919ac67b54a856bd6f655d52d801c1ebbb07dd
ahmedk <ahmed.m.kamel@nileu.edu.eg>
2012-12-29 22:24:37 +0200

    Added some tests

--- spec/controllers/car_controller_spec.rb
+++ spec/controllers/car_controller_spec.rb
@@ -10,10 +10,33 @@ describe CarController do
   end
 
   describe "GET 'details'" do
-    it "should display car page" do
+		it "should load correct car" do
       Car.should_receive(:find).with(1)
       get 'details', :id => 1
     end
+
+    it "should display car page" do
+			get 'details', :id => 1
+			page.should have_content('Details')
+		end
   end
 
+	describe "Get 'new'" do
+		it "should display new car page" do
+			get 'new'
+			page.should have_content('Edit')
+		end
+	end
+
+  describe "Get 'edit'" do
+    it "should load correct car for edit page" do
+      Car.should_receive(:find).with(1)
+			get 'edit', :id => 1
+    end
+
+    it "should display edit page" do
+			get 'edit', :id => 1
+			page.should have_content('Edit')
+		end
+  end
 end
